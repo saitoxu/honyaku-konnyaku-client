b033d8d28960ee1233ba33f300f8956dca282e8a
Yosuke Saito <yosuke.saito1202@gmail.com>
2016-11-20 01:04:51 +0900

    create project

---
+++ honyaku-konnyaku-client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  honyaku-konnyaku-client
+//
+//  Created by Yosuke SAITO on 2016/11/19.
+//  Copyright © 2016年 saitoxu. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import Speech
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
